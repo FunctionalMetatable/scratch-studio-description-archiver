9079aaa8d9c560b3cd474625497e87c94291c596
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-02 15:07:50 +0100

    Remove last edited feature

--- src/index.js
+++ src/index.js
@@ -8,17 +8,6 @@ fs.readFile("./description.txt", "utf-8", (err, oldDescription) => {
   .then(studioJson => {
     if (studioJson.description !== oldDescription) {
       fs.writeFileSync("./description.txt", studioJson.description)
-      fetch(`https://api.scratch.mit.edu/studios/${STUDIO_ID}/activity?limit=40`)
-      .then(res => res.json())
-      .then(activity => {
-        for (let k=0;k<activity.length;k++) {
-          let activityItem = activity[k]
-          if (activityItem.type !== 'updatestudio') {
-            continue
-          }
-          fs.writeFileSync("./last-edited.txt", activityItem.actor_username)
-        }
-      })
     } else {
       console.log("No new changes to the description")
     }
