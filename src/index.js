61e7bfa0759f5567aafbc341e66b1b8f679d7cbd
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-09-02 09:35:51 +0100

    rm -rf node-fetch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 const STUDIO_ID = 26371714; // Replace 0 with your studio id
 const fs = require("fs")
-const fetch = require("node-fetch")
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    const urlobj = new URL(url);
+    const req = https
+      .request(urlobj, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+
+        res.on("error", (err) => {
+          reject(err);
+        });
+      })
+      .end();
+  });
+}
 
 fs.readFile("./description.txt", "utf-8", (err, oldDescription) => {
-  fetch(`https://api.scratch.mit.edu/studios/${STUDIO_ID}/`)
-  .then(res => res.json())
+  getJSON(`https://api.scratch.mit.edu/studios/${STUDIO_ID}/`)
   .then(studioJson => {
     if (studioJson.description !== oldDescription) {
       fs.writeFileSync("./description.txt", studioJson.description)
